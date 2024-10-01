be4ccaa616da1beec0f99b8cef783184423005a7
elissandroa <elissandroa@hotmail.com>
2024-10-01 13:58:58 -0300

    Adicionando os Eventos e Transição nos Cards da Plicação com JavaScript

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,20 @@
+function handleMouseEnter () {
+    this.classList.add('s-card--hovered');
+}
+
+function handleMouseLeave() {
+    this.classList.remove('s-card--hovered');
+}
+
+function addEnventListenersTocard() {
+    const cardElements = document.getElementsByClassName("s-card");
+
+    for (let i = 0; cardElements.length; i++){
+        const card  = cardElements[i];
+        card.addEventListener('mouseenter',  handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave)
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", addEnventListenersTocard, false);
